f5071dfc11e35c67b924d875d1fd73aee736e9f9
aersosi <aersosi@avodaq.com>
2021-12-05 13:07:01 +0100

    [init]

--- functional-css-data-attr/src/index.test.js
+++ functional-css-data-attr/src/index.test.js
@@ -1,6 +1,6 @@
 const postcss = require('postcss')
 
-const plugin = require('./')
+const plugin = require('./index')
 
 async function run (input, output, opts = { }) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
